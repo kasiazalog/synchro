d3c25809e53779f30ceef1955e72a45983c3373d
Kasia Zalog <kasia.zalog@gmail.com>
2014-02-28 13:09:40 +0100

    Sign-up-in-out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Synchro::Application.config.secret_key_base = '4fb8352f7d9b52a07d9c1698b34dd13c4a7f939dc42e43d4907ee496741cc7d8b3e145192fd8aded344dece4872ca834c4f3af6af7590279d612fba882c4845c'
